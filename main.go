3f7f7652228eeefda3eae12b0a333f8fcbf60acf
codedbyshoe <andrew.shoemaker9@gmail.com>
2024-10-02 17:39:29 -0500

    refactored my php like mess to structual typing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
+	"reflect"
 	"sync"
 
 	"github.com/gdamore/tcell/v2"
@@ -22,153 +23,216 @@ type SearchResult struct {
 	Data []NuGetPackage `json:"data"`
 }
 
-var (
-	searchResults []NuGetPackage
-	mu            sync.Mutex
-)
+type PackageManager struct {
+	App           *tview.Application
+	List          *tview.List
+	Input         *tview.InputField
+	Details       *tview.TextView
+	Installed     *tview.TextView
+	Layout        *tview.Flex
+	Url           string
+	SearchResults []NuGetPackage
+	Mu            sync.Mutex
+}
 
-func main() {
-	app := tview.NewApplication()
+func NewPackageManager(app *tview.Application, url string) *PackageManager {
+	pm := &PackageManager{
+		App:           app,
+		List:          tview.NewList(),
+		Input:         tview.NewInputField(),
+		Details:       tview.NewTextView(),
+		Installed:     tview.NewTextView(),
+		Layout:        tview.NewFlex(),
+		Url:           url,
+		SearchResults: []NuGetPackage{},
+		Mu:            sync.Mutex{},
+	}
+	return pm
+}
 
-	// create the main lyout
-	layout := tview.NewFlex().SetDirection(tview.FlexRow)
+// build out App UI
 
-	// create a list area for the search results
-	list := tview.NewList().ShowSecondaryText(false)
-	list.SetBorder(true).SetTitle("Nuget Pagkages")
-	list.SetBackgroundColor(tcell.ColorDefault)
+func (pm *PackageManager) buildList() {
+	pm.List.ShowSecondaryText(false)
+	pm.List.SetBorder(true).SetTitle("Nuget Pagkages")
+	pm.List.SetBackgroundColor(tcell.ColorDefault)
+}
 
-	// create the input for search
-	input := tview.NewInputField()
-	input.SetBackgroundColor(tcell.ColorDefault)
-	input.SetFieldBackgroundColor(tcell.ColorDefault)
-	input.SetBorder(true).SetTitle("Search Package By Name").SetBorderPadding(0, 0, 1, 1)
+func (pm *PackageManager) buildInput() {
+	pm.Input.SetBackgroundColor(tcell.ColorDefault)
+	pm.Input.SetFieldBackgroundColor(tcell.ColorDefault)
+	pm.Input.SetBorder(true).
+		SetBorderColor(tcell.ColorGreen).
+		SetTitle("Search Package By Name").
+		SetBorderPadding(0, 0, 1, 1)
+}
 
-	// create the package details view
-	details := tview.NewTextView().SetDynamicColors(true)
-	details.SetBorder(true).SetTitle("Package Details")
-	details.SetBackgroundColor(tcell.ColorDefault)
+func (pm *PackageManager) buildDetails() {
+	pm.Details.SetDynamicColors(true)
+	pm.Details.SetBorder(true).SetTitle("Package Details")
+	pm.Details.SetBackgroundColor(tcell.ColorDefault)
+}
 
-	installed := tview.NewTextView().SetDynamicColors(true)
-	installed.SetBorder(true).SetTitle("Installed Packages")
-	installed.SetBackgroundColor(tcell.ColorDefault)
+func (pm *PackageManager) buildInstalled() {
+	pm.Installed.SetDynamicColors(true)
+	pm.Installed.SetBorder(true).SetTitle("Installed Packages")
+	pm.Installed.SetBackgroundColor(tcell.ColorDefault)
+}
+
+func (pm *PackageManager) buildLayout() {
+	pm.Layout.SetDirection(tview.FlexRow)
+	pm.buildList()
+	pm.buildInput()
+	pm.buildDetails()
+	pm.buildInstalled()
 
 	right := tview.NewFlex().
 		SetDirection(tview.FlexRow).
-		AddItem(details, 0, 1, false).
-		AddItem(installed, 0, 1, false)
+		AddItem(pm.Details, 0, 1, false).
+		AddItem(pm.Installed, 0, 1, false)
 
 	// Add Components to the layout
-	layout.AddItem(tview.NewFlex().
-		AddItem(list, 0, 1, false).
-		AddItem(right, 0, 1, false),
-		0, 3, false)
-	layout.AddItem(input, 3, 1, true)
-	layout.SetBackgroundColor(tcell.ColorDefault)
+	pm.Layout.AddItem(tview.NewFlex().
+		AddItem(pm.List, 0, 1, false).
+		AddItem(right, 0, 1, false), 0, 3, false)
 
-	input.SetDoneFunc(func(key tcell.Key) {
+	pm.Layout.AddItem(pm.Input, 3, 1, true)
+	pm.Layout.SetBackgroundColor(tcell.ColorDefault)
+}
+
+func (pm *PackageManager) setFocus(from, to interface{}) {
+	if p, ok := to.(tview.Primitive); ok {
+		pm.App.SetFocus(p)
+	}
+
+	// Helper function to set border color  NOTE: There is probably a better way to do this
+	setBorderColorIfExists := func(item interface{}, color tcell.Color) {
+		v := reflect.ValueOf(item)
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+		}
+		if v.Kind() != reflect.Struct {
+			return
+		}
+		method := v.MethodByName("SetBorderColor")
+		if method.IsValid() && !method.IsNil() {
+			method.Call([]reflect.Value{reflect.ValueOf(color)})
+		}
+	}
+
+	// Set border colors
+	setBorderColorIfExists(from, tcell.ColorDefault)
+	setBorderColorIfExists(to, tcell.ColorGreen)
+}
+
+// create app navigations
+
+func (pm *PackageManager) handleInputSearch() {
+	pm.Input.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
-			query := input.GetText()
-			list.Clear()
-			details.Clear()
-			input.SetText("")
-			list.AddItem("Searching Packages...", "", 0, nil)
-			details.SetText("Gathering Package Details...")
-			app.SetFocus(list)
-			go searchPackages(query, list, details, app)
+			query := pm.Input.GetText()
+			pm.List.Clear()
+			pm.Details.Clear()
+			pm.Input.SetText("")
+			pm.List.AddItem("Searching Packages...", "", 0, nil)
+			pm.Details.SetText("Gathering Package Details...")
+			pm.App.SetFocus(pm.List)
+			pm.Input.SetBorderColor(tcell.ColorDefault)
+			pm.List.SetBorderColor(tcell.ColorGreen)
+			go pm.searchPackages(query)
 		}
 	})
+}
 
-	// Show installed packages from csproj
-	listInstalledPackages(installed)
-
+func (pm *PackageManager) handleAppNavigation() {
 	// handle key events
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	pm.App.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
 		case tcell.KeyTab:
-			switch app.GetFocus() {
-			case input:
-				app.SetFocus(list)
-				input.SetBorderColor(tcell.ColorDefault)
-				list.SetBorderColor(tcell.ColorGreen)
-			case list:
-				app.SetFocus(input)
-				list.SetBorderColor(tcell.ColorDefault)
-				input.SetBorderColor(tcell.ColorGreen)
+			switch pm.App.GetFocus() {
+			case pm.Input:
+				pm.setFocus(pm.Input, pm.List)
+			case pm.List:
+				pm.setFocus(pm.List, pm.Input)
 			}
 			return nil
 		case tcell.KeyBacktab:
-			switch app.GetFocus() {
-			case input:
-				app.SetFocus(list)
-				input.SetBorderColor(tcell.ColorDefault)
-				list.SetBorderColor(tcell.ColorGreen)
-			case list:
-				app.SetFocus(input)
-				list.SetBorderColor(tcell.ColorDefault)
-				input.SetBorderColor(tcell.ColorGreen)
+			switch pm.App.GetFocus() {
+			case pm.Input:
+				pm.setFocus(pm.Input, pm.List)
+			case pm.List:
+				pm.setFocus(pm.List, pm.Input)
 			}
 			return nil
 		case tcell.KeyEscape:
-			app.Stop()
+			pm.App.Stop()
 		}
 
-		if app.GetFocus() == list {
+		if pm.App.GetFocus() == pm.List {
 			switch event.Rune() {
 			case 'j':
-				currentIndex := list.GetCurrentItem()
-				if currentIndex < list.GetItemCount()-1 {
-					navigateList(currentIndex+1, list, details)
+				currentIndex := pm.List.GetCurrentItem()
+				if currentIndex < pm.List.GetItemCount()-1 {
+					pm.navigateList(currentIndex + 1)
 				}
 				return nil
 			case 'k':
-				currentIndex := list.GetCurrentItem()
+				currentIndex := pm.List.GetCurrentItem()
 				// get the current item id
 				if currentIndex > 0 {
-					navigateList(currentIndex-1, list, details)
+					pm.navigateList(currentIndex - 1)
 				}
 				return nil
 			}
 		}
 		return event
 	})
-
-	if err := app.SetRoot(layout, true).EnableMouse(true).Run(); err != nil {
-		panic(err)
-	}
 }
 
-func listInstalledPackages(installed *tview.TextView) {
+// build out the application
+
+func (pm *PackageManager) Run() {
+	pm.buildLayout()
+	pm.handleInputSearch()
+	pm.listInstalledPackages()
+	pm.handleAppNavigation()
+
+	pm.App.SetRoot(pm.Layout, true).EnableMouse(true)
+}
+
+// methods for app logic
+
+func (pm *PackageManager) listInstalledPackages() {
 	cmd := exec.Command("dotnet", "list", "package")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		installed.Clear()
-		installed.SetText("No dependencies found. \n Are you in a project folder?")
+		pm.Installed.Clear()
+		pm.Installed.SetText("No dependencies found. \n Are you in a project folder?")
 		return
 	}
-	installed.SetText(string(output))
+	pm.Installed.SetText(string(output))
 }
 
-func navigateList(idx int, list *tview.List, details *tview.TextView) {
-	list.SetCurrentItem(idx)
-	id, _ := list.GetItemText(idx)
-	for k, v := range searchResults {
+func (pm *PackageManager) navigateList(idx int) {
+	pm.List.SetCurrentItem(idx)
+	id, _ := pm.List.GetItemText(idx)
+	for k, v := range pm.SearchResults {
 		if v.ID == id {
-			pkg := searchResults[k]
-			go showPackageDetails(pkg, details)
+			pkg := pm.SearchResults[k]
+			go pm.showPackageDetails(pkg)
 		}
 	}
 }
 
-func searchPackages(query string, list *tview.List, details *tview.TextView, app *tview.Application) {
-	url := fmt.Sprintf("https://api-v2v3search-0.nuget.org/query?q=%s&take=100&includeDelisted=false", query)
+func (pm *PackageManager) searchPackages(query string) {
+	url := fmt.Sprintf(pm.Url, query)
 
 	resp, err := http.Get(url)
 	if err != nil {
 		// Handle error
-		app.QueueUpdateDraw(func() {
-			list.Clear()
-			details.SetText(fmt.Sprintf("Error fetching packages. Try again. \n Error: %v", err))
+		pm.App.QueueUpdateDraw(func() {
+			pm.List.Clear()
+			pm.Details.SetText("Error fetching packages. \n Check your connection.")
 		})
 		return
 	}
@@ -178,34 +242,45 @@ func searchPackages(query string, list *tview.List, details *tview.TextView, app
 
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		// Handle error
-		app.QueueUpdateDraw(func() {
-			list.Clear()
-			details.SetText(fmt.Sprintf("Error decoding response. \n Error: %v", err))
+		pm.App.QueueUpdateDraw(func() {
+			pm.List.Clear()
+			pm.Details.SetText(fmt.Sprintf("Error decoding response. \n Error: %v", err))
 		})
 		return
 	}
 
-	mu.Lock()
-	searchResults = result.Data
-	mu.Unlock()
+	pm.Mu.Lock()
+	pm.SearchResults = result.Data
+	pm.Mu.Unlock()
 
-	app.QueueUpdateDraw(func() {
-		list.Clear()
+	pm.App.QueueUpdateDraw(func() {
+		pm.List.Clear()
 		for _, pkg := range result.Data {
-			list.AddItem(pkg.ID, "", 0, nil)
+			pm.List.AddItem(pkg.ID, "", 0, nil)
 		}
-		if len(searchResults) > 0 {
-			list.SetCurrentItem(0)
-			showPackageDetails(searchResults[0], details)
+		if len(pm.SearchResults) > 0 {
+			pm.List.SetCurrentItem(0)
+			pm.showPackageDetails(pm.SearchResults[0])
 		}
 	})
 }
 
-func showPackageDetails(pkg NuGetPackage, details *tview.TextView) {
+func (pm *PackageManager) showPackageDetails(pkg NuGetPackage) {
 	detailsText := fmt.Sprintf("Package Id: %s\n", pkg.ID)
 	detailsText += fmt.Sprintf("Version: %s\n", pkg.Version)
 	detailsText += fmt.Sprintf("Total downloads: %d\n", pkg.TotalDownloads)
 	detailsText += fmt.Sprintf("Description: %s\n", pkg.Description)
 
-	details.SetText(detailsText)
+	pm.Details.SetText(detailsText)
+}
+
+func main() {
+	app := tview.NewApplication()
+
+	packageManagerApp := NewPackageManager(app, "https://api-v2v3search-0.nuget.org/query?q=%s&take=100&includeDelisted=false")
+	packageManagerApp.Run()
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
 }
